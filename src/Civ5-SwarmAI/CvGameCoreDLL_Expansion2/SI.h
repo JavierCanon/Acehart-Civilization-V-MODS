d92c0eadeaf00d2c64f5981fdca939504a92005a
Javier Cañon <javier@javiercanon.com>
2020-05-17 14:00:56 -0500

    start working in AI

---
+++ src/Civ5-SwarmAI/CvGameCoreDLL_Expansion2/SI.h
@@ -0,0 +1,20 @@
+// SI.h
+// Used to keep track of modified functions throughout the code
+
+#pragma once
+
+#ifndef SI_MODS_H
+#define SI_MODS_H
+
+// Random needed stuff
+
+// SI mod new GUID
+#define SI_GUID
+
+// SI mod new Tactical AI constructor
+// #define SI_NEW_TACTICAL_AI_INIT
+
+// Properly destroy 2D arrays, credit to Delnar_Ersike
+#define AUI_DATABASE_UTILITY_PROPER_2D_ALLOCATION_AND_DESTRUCTION
+
+#endif
